7dd7bda09a43febbef606c0f75a8e73d07a6c1cc
eSeohyeon <leeseohyeon1203@gmail.com>
2024-12-09 22:44:48 +0900

    fix: 홈 그리팅 수정, 생체데이터 레이아웃 수정

--- app/src/main/java/com/example/oncare/HomeFragment.java
+++ app/src/main/java/com/example/oncare/HomeFragment.java
@@ -13,9 +13,9 @@ import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
 
-    TextView txt_name, data_heartrate, data_step, data_ecg, nim;
+    TextView data_heartrate, data_step, data_ecg, nim;
     int heartrate, step; // 심박수, 걸음수 저장
-    String ecg, user_name;// 심전도 데이터, 사용자 이름 데이터 저장
+    String ecg;// 심전도 데이터, 사용자 이름 데이터 저장
     Button btn_test;
     EmergencyDialog emergencyDialog;
     ConfirmDialog confirmDialog;
@@ -35,16 +35,14 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_home, container, false);
 
-        txt_name=v.findViewById(R.id.user_name);
-        nim=v.findViewById(R.id.nim);
+
         data_heartrate=v.findViewById(R.id.data_heartrate);
         data_step=v.findViewById(R.id.data_step);
         data_ecg=v.findViewById(R.id.data_ecg);
         tab_step=v.findViewById(R.id.tab_step);
-        txt_name.bringToFront();
-        nim.bringToFront();
 
         btn_test=v.findViewById(R.id.btn_test);
+        btn_test.setVisibility(View.INVISIBLE);
 
         btn_test.setOnClickListener(new View.OnClickListener() { // 위험신호 감지 팝업 테스트용
             @Override
@@ -78,9 +76,8 @@ public class HomeFragment extends Fragment {
         heartrate=120;
         step=7878;
         ecg="부정맥";
-        user_name="김김김김";  // 테스트용
+        // 테스트용
 
-        txt_name.setText(user_name);
         data_heartrate.setText(Integer.toString(heartrate));
         data_step.setText(Integer.toString(step));
         data_ecg.setText(ecg);
